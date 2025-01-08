74136a6738aa34412271692e880fa78eb1058956
TobiasgutPrivat <79203201+TobiasgutPrivat@users.noreply.github.com>
2025-01-08 09:17:32 +0100

    Task concept simplification

--- comparison Concepts.ts
+++ comparison Concepts.ts
@@ -1,31 +1,28 @@
 import mineflayer from "mineflayer";
 
 export interface Task {
-    isSatisfied(bot: mineflayer.Bot): boolean; // Whether the task is already complete.
-    canRun(bot: mineflayer.Bot): boolean; // Whether the task can be executed immediately.
-    getSubtasks(bot: mineflayer.Bot): Task[]; // Subtasks needed before running.
-    run(bot: mineflayer.Bot): Promise<void>; // Execute the task.
+    tasks: Task[]; //repressents ways to satisfy the requirement
+    isSatisfied(bot: mineflayer.Bot): boolean;
+    canRun(bot: mineflayer.Bot): boolean; // true if all requirements are satisfied
+    run(bot: mineflayer.Bot, finishcallback?: () => void): void; // Execute the task.
 }
 
 // Task haveStonepickaxe:
 // isSatisfied: check if the player has a pickaxe
 // -> if not craft pickaxe
-// subTasks: haveStone, havesticks, haveplacedcraftingtable
-// canRun: haveStone && haveSticks && haveplacedcraftingtable
+// tasks: haveStone, havesticks, haveplacedcraftingtable
 // run: craft
 
 // Task haveStone:
 // isSatisfied: check if the player has a stone
 // -> if not mine stone
-// subTasks: havepickaxe
-// canRun: havepickaxe
+// tasks: havepickaxe
 // run: mine
 
 // Task haveSticks:
 // isSatisfied: check if the player has a sticks
 // -> if not mine leaves || craft sticks //TODO how to represent multiple ways
-// subTasks: None || haveWoodenPlanks 
-// canRun: True || haveWoodenPlanks
+// tasks: None || haveWoodenPlanks 
 // run: mine || craft
 
 export interface Requirement {
