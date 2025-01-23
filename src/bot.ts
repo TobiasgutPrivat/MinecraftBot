8967f60fe74d4915419146ac94617a5c237d7c77
TobiasgutPrivat <79203201+TobiasgutPrivat@users.noreply.github.com>
2025-01-23 15:15:08 +0100

    Async calculation

--- src/bot.ts
+++ src/bot.ts
@@ -9,6 +9,7 @@ export default class Bot {
     readonly bot: mineflayer.Bot
     goals: [number,(botState: BotState) => number][] = [] //list of [weight, rateGoal()] tuples
     private tpsScoreboard?: TpsScoreboard;
+    private isEvaluating: boolean = false
     //maybe make bot private
 
     constructor(name: string) {
@@ -32,67 +33,68 @@ export default class Bot {
         this.bot.once("spawn", () => {
             const defaultMove = new Movements(this.bot);
             this.bot.pathfinder.setMovements(defaultMove);
-            this.mainLoop();
             this.tpsScoreboard = new TpsScoreboard(this.bot);
+
+            this.bot.on("physicsTick", () => {
+                this.tpsScoreboard?.tick();
+                if (this.isEvaluating) return
+                this.isEvaluating = true
+                this.reEvaluateActions().then(() => this.isEvaluating = false)
+            })
         });
     }
 
-    //always reEvaluate, max 1 time per tick
-    private async mainLoop() {
-        while (true) {
-            this.tpsScoreboard?.tick();
-            this.reEvaluateActions()
-            await this.bot.waitForTicks(1)
-        }
-    }
+    private async reEvaluateActions(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (!this.currentaction?.isRunning(this.bot)) this.currentaction = undefined
 
-    private reEvaluateActions() {
-         if (!this.currentaction?.isRunning(this.bot)) this.currentaction = undefined
+            const currentBotState = new BotState(this.bot)
+            const currentScore = this.BotStateScore(currentBotState)
 
-        const currentBotState = new BotState(this.bot)
-        const currentScore = this.BotStateScore(currentBotState)
+            const actionSuggestions = currentBotState.actionSuggestions.filter(action => action.canRun(this.bot))
 
-        const actionSuggestions = currentBotState.actionSuggestions.filter(action => action.canRun(this.bot))
+            // actionScores = [action, scoreImprovement, effort]
+            var actionScores: [Action, number, number][] = actionSuggestions.map(action => 
+                {
+                    const actionScore = this.ActionScore(action)
+                    const actionEffort = action.getEffort(this.bot)
+                    return [action, actionScore - currentScore, actionEffort] //note: for progress stuff without other goals diffrence should be similair to effort
+                }
+            )
+            // maybe include wait Action
 
-        // actionScores = [action, scoreImprovement, effort]
-        var actionScores: [Action, number, number][] = actionSuggestions.map(action => 
-            {
-                const actionScore = this.ActionScore(action)
-                const actionEffort = action.getEffort(this.bot)
-                return [action, actionScore - currentScore, actionEffort] //note: for progress stuff without other goals diffrence should be similair to effort
+            if (actionScores.length == 0) return
+            
+            const sortedAction = actionScores.sort((a, b) => {
+                const diffrence = (b[1]/b[2]) - (a[1]/a[2])
+                if (Math.abs(diffrence) < 0.001) {
+                    return diffrence
+                } else {
+                    return a[2] - b[2]
+                }
+            }) //compare score/effort
+
+            const minImprovement = 0.1
+
+            var bestAction = sortedAction[0][0]
+
+            if (this.currentaction) {
+                const currentEval = this.ActionScore(this.currentaction) - currentScore / this.currentaction.getEffort(this.bot)
+                const bestEval = sortedAction[0][1] / sortedAction[0][2]
+                if (bestEval - currentEval < minImprovement) {
+                    bestAction = this.currentaction
+                }
             }
-        )
-        // maybe include wait Action
 
-        if (actionScores.length == 0) return
-        
-        const sortedAction = actionScores.sort((a, b) => {
-            const diffrence = (b[1]/b[2]) - (a[1]/a[2])
-            if (Math.abs(diffrence) < 0.001) {
-                return diffrence
-            } else {
-                return a[2] - b[2]
+            if (!this.currentaction || bestAction.id !== this.currentaction.id) {
+                this.currentaction?.stop(this.bot) //maybe not needed
+                this.currentaction = bestAction
+                this.bot.chat("Running " + this.currentaction.id)
+                this.currentaction.run(this.bot) // test what happens if currentaction already changed, or just don't remove
             }
-        }) //compare score/effort
 
-        const minImprovement = 0.1
-
-        var bestAction = sortedAction[0][0]
-
-        if (this.currentaction) {
-            const currentEval = this.ActionScore(this.currentaction) - currentScore / this.currentaction.getEffort(this.bot)
-            const bestEval = sortedAction[0][1] / sortedAction[0][2]
-            if (bestEval - currentEval < minImprovement) {
-                bestAction = this.currentaction
-            }
-        }
-
-        if (!this.currentaction || bestAction.id !== this.currentaction.id) {
-            this.currentaction?.stop(this.bot) //maybe not needed
-            this.currentaction = bestAction
-            this.bot.chat("Running " + this.currentaction.id)
-            this.currentaction.run(this.bot) // test what happens if currentaction already changed, or just don't remove
-        }
+            resolve()
+        })
     }
 
     private BotStateScore(botState: BotState): number {
